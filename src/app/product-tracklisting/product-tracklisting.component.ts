49ccf2d1db1a14627c96448d17ed31fd8cd9f6de
Fernando Mejia <fernandoemejia@yahoo.com>
2020-07-15 16:03:18 -0400

    Completed module 4

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {Album} from '../album';
-
+import {ProductService} from '../product.service'
 @Component({
   selector: 'app-product-tracklisting',
   templateUrl: './product-tracklisting.component.html',
